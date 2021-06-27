cbeefd935cf3ca0c73d165becf9cc7904bebf68e
jens harms <au1064@gmail.com>
2021-06-27 13:40:04 +0200

    ADD speed test history file

--- RUN.sh
+++ RUN.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-grep -m 1 model\ name /proc/cpuinfo 
-make >/dev/null && ./svar2-test
+
+make >/dev/null || exit 1
+cpu=$(grep -m 1 model\ name /proc/cpuinfo)
+speed=$(./svar2-test)
+today=$(date +%s)
+
+echo $today : $speed : $cpu | tee -a speed-test.txt
+
